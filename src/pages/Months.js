3fd92a284651b2f2bbbbcb7222971e8f412acb6b
khoatr1509 <retdkhoa@gmail.com>
2023-01-02 16:25:03 +0700

    Update maycha application

--- src/pages/Months.js
+++ src/pages/Months.js
@@ -1,131 +1,88 @@
-import React from "react";
+import React, { useState } from "react";
+import { useEffect } from "react";
 import {
     BrowserRouter as Router,
     Switch,
     Route,
-    useRouteMatch,
-    useParams
+    useRouteMatch
 } from "react-router-dom";
 
-import { ref, getDownloadURL } from "firebase/storage";
-
-
+import { ref, listAll, getDownloadURL} from "firebase/storage";
 import { storage } from "../firebase";
 import notes from '../data/data';
 
 let [...arr] = notes;
 
-
-const listRef = ref(storage, 'rec');
-
-
-// Find all the prefixes and items.
-// listAll(listRef)
-//     .then((res) => {
-//         res.prefixes.forEach((folderRef) => {
-//             // All the prefixes under listRef.
-//             // You may call listAll() recursively on them.
-//         });
-//         res.items.forEach((itemRef) => {
-//             // All the items under listRef.
-//             console.log(itemRef);
-//         });
-//     }).catch((error) => {
-//         // Uh-oh, an error occurred!
-//     });
+const listRef = ref(storage, 'rec/');
 
 export const Months = () => {
     let match = useRouteMatch();
-    let url = getDownloadURL(ref(storage, 'rec/Business-Result-04-2022.pdf'))
-        .then((url) => {
-            // `url` is the download URL for 'images/stars.jpg'
-            // console.log("here");
-            // console.log(url);
-            return <h3>Requested topic ID: {url}</h3>;
-            // // This can be downloaded directly:
-            // const xhr = new XMLHttpRequest();
-            // xhr.responseType = 'blob';
-            // xhr.onload = (event) => {
-            //     const blob = xhr.response;
-            // };
-            // xhr.open('GET', url);
-            // xhr.send();
+    const [listPdf, setPdf] = useState([]);
 
-            // // Or inserted into an <img> element
-            // const img = document.getElementById('myimg');
-            // img.setAttribute('src', url);
-        })
-        .catch((error) => {
-            // Handle any errors
-        });
+    useEffect(() => {
+        listAll(listRef)
+            .then(async (res) => {
+                Promise.all(
+                    res.items.map((itemRef) => {
+                        return getDownloadURL(ref(storage, itemRef.fullPath))
+                    })
+                ).then((urls) => {
+                    console.log(urls)
+                    setPdf(urls)
+                })
+            })
+    }, [])
+    // console.log('https://firebasestorage.googleapis.com/v0/b/rec-app-803db.appspot.com/o/rec%2'.length)
+    // console.log(listPdf[0])
+    let a = Array(3 - listPdf.length % 3 + 3).fill(0);
+    // console.log(a)
+    console.log(listPdf[0]);
     return (
         <Router>
             <Switch>
-                {/* <Route path={`${match.url}/:topicId`}>
-                    <Topic />
-                </Route> */}
                 <Route path={`${match.path}`}>
                     <h2 className="text-3xl my-4 md:my-8 md:text-5xl font-extrabold text-red-600 m-auto mt-4 w-[300px] md:w-[460px] text-center">MAYCHA <br></br> BUSINESS RESULT</h2>
 
                     <div className=" mx-auto flex row flex-wrap items-center justify-center mobile:w-[381px] medium-mobile:w-[393px] mobile:mx-auto h-fit sm:w-[80%] sm:items-start md:justify-start">
-                            {arr.map(i => {
-                                // console.log(i.length >= 10 ? i.id : `0${i.id}`)
-                                if (i.id <= 12) {
+                        {listPdf.map(i => {
+                            let url = i;
+                            const [a, b, month, year] = i.slice(78, 101).split('-');
+                            if (parseInt(month) <= 12) {
                                     return (
                                         <a className=" mobile:w-[126px] mobile:h-[126px] medium-mobile:w-[130px] medium-mobile:h-[130px] md:w-[140px] md:h-[140px] flex items-center justify-center flex-col sm:py-2 px-2 sm:px-4 text-center bg-transparent mr-0 mt-0 font-bold border-black border-[1px] rounded-lg bg-red-600 text-white hover:border-transparent transition ease-in duration-200 transform hover:-translate-y-1 active:translate-y-0 "
-                                            href={`https://firebasestorage.googleapis.com/v0/b/rec-app-803db.appspot.com/o/rec%2FBusiness-Result-${i.id >= 10 ? i.id : `0${i.id}`}-2022.pdf?alt=media&token=95ece86a-0137-496b-b5f0-cbd3a603dfbc`}
-                                            target="_blank" rel="noreferrer"
+                                            href={url}
                                         >
-                                            <p className=" w-full">Tháng {i.id}</p>
+                                            <p className=" w-full">Tháng {month}</p>
                                             <p>2022</p>
                                         </a>
                                     )
+                                } else {
+                                    return (
+                                        <a className=" mobile:w-[126px] mobile:h-[126px] medium-mobile:w-[130px] medium-mobile:h-[130px] md:w-[140px] md:h-[140px] flex items-center justify-center flex-col sm:py-2 px-2 sm:px-4 text-center bg-transparent mr-0 mt-0 font-bold border-black border-[1px] rounded-lg bg-red-600 text-white hover:border-transparent transition ease-in duration-200 transform hover:-translate-y-1 active:translate-y-0 "
+                                            href={url}
+                                        >
+                                            <p className=" w-full">Tháng {month % 12}</p>
+                                            <p>2023</p>
+                                        </a>
+                                    )
                                 }
-                                return (<a className=" mobile:w-[126px] mobile:h-[126px] medium-mobile:w-[130px] medium-mobile:h-[130px] md:w-[140px] md:h-[140px] flex items-center justify-center flex-col sm:py-2 px-2 sm:px-4 text-center bg-transparent mr-0 mt-0 font-bold border-black border-[1px] rounded-lg bg-slate-50 text-gray-300 hover:border-transparent transition ease-in duration-200 transform hover:-translate-y-1 active:translate-y-0 "
+                        })}
+                        {
+                            a.map((i) => {
+                                return (
+                                    <a className=" mobile:w-[126px] mobile:h-[126px] medium-mobile:w-[130px] medium-mobile:h-[130px] md:w-[140px] md:h-[140px] flex items-center justify-center flex-col sm:py-2 px-2 sm:px-4 text-center bg-transparent mr-0 mt-0 font-bold border-black border-[1px] rounded-lg bg-slate-50 text-gray-300 hover:border-transparent transition ease-in duration-200 transform hover:-translate-y-1 active:translate-y-0 "
                                     href={`/months`}
                                     target="_blank" rel="noreferrer"
                                 >
-                                    <p className=" w-full">Tháng {i.id < 12 ? i.id : i.id % 12}</p>
-                                    <p>2023</p>
+                                        <p className=" w-full">Tháng</p>
+                                        <p>2023</p>
                                 </a>)
-
-                            })}
+                            })
+                        }
                     </div>
                 </Route>
             </Switch>
         </Router >
 
     )
-}
-function Topic() {
-    let { topicId } = useParams();
-    console.log(topicId)
-    let i = topicId.length == 2 ? topicId : "0" + topicId;
-    return <a href={`https://firebasestorage.googleapis.com/v0/b/rec-app-803db.appspot.com/o/rec%2FBusiness-Result-${i}-2022.pdf?alt=media&token=95ece86a-0137-496b-b5f0-cbd3a603dfbc`}>Link</a>
-}
-function Month() {
-    let { topicId } = useParams();
-
-    getDownloadURL(ref(storage, 'rec/Business-Result-01-2022.pdf'))
-        .then((url) => {
-            // `url` is the download URL for 'images/stars.jpg'
-            console.log("here");
-            console.log(url);
-            return <h3>Requested topic ID: {url}</h3>;
-            // // This can be downloaded directly:
-            // const xhr = new XMLHttpRequest();
-            // xhr.responseType = 'blob';
-            // xhr.onload = (event) => {
-            //     const blob = xhr.response;
-            // };
-            // xhr.open('GET', url);
-            // xhr.send();
-
-            // // Or inserted into an <img> element
-            // const img = document.getElementById('myimg');
-            // img.setAttribute('src', url);
-        })
-        .catch((error) => {
-            // Handle any errors
-        });
 }
